179db35ce428208c44bb7fc6215a4bf38ad74601
Nicolas Lecoy <nicolas.lecoy@gmail.com>
2025-11-15 09:26:01 +0100

    refactor(agent): use cihub runner

--- agent/agent.go
+++ agent/agent.go
@@ -184,7 +184,7 @@ func (a *Agent) Run(ctx context.Context, runner *core.Runner) error {
 	metadata := map[string]interface{}{
 		"latest": map[string]interface{}{
 			"meta-data": map[string]interface{}{
-				"fireactions": map[string]interface{}{
+				"cihub": map[string]interface{}{
 					"runner_id":         runner.Name,
 					"runner_jit_config": jit.Token,
 				},
